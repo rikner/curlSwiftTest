81054457e0644fc6eccac4dc5a94a284004b722c
rikner <rikner@web.de>
2017-09-27 17:58:06 +0200

    get query working using utf8 string pointer

--- Sources/cCurlTest/main.swift
+++ Sources/cCurlTest/main.swift
@@ -18,7 +18,6 @@ guard let curl = curl_easy_init() else {
 }
 
 let serverUrl =  "http://192.168.1.10:3000/graphql"
-// let postData = "{\"query\": \"query SingleSong($songId: ID!) { song(songId: $songId) { _id } }\", \"variables\": {\"songId\": \"AqpMMhAgBvw4u9qsu\"}}"
 let postData = """
 {
 	"query": "query SingleSong($songId: ID!) { song(songId: $songId) { _id } }",
@@ -31,13 +30,21 @@ let postData = """
 // set url
 curlHelperSetOptString(curl, CURLOPT_URL, serverUrl)
 
-// set data
-curlHelperSetOptString(curl, CURLOPT_POSTFIELDS, postData)
+// set post true
+curlHelperSetOptBool(curl, CURLOPT_POST, CURL_TRUE)
 
 // set headers
 var headers = curl_slist_append(nil, "Content-Type: application/json")
-// headers = curl_slist_append(headers, "charsets: utf-8")
+headers = curl_slist_append(headers, "charsets: utf-8")
 curlHelperSetOptList(curl, CURLOPT_HTTPHEADER, headers)
+
+// set post data
+// curlHelperSetOptString(curl, CURLOPT_POSTFIELDS, postData)
+
+let postDataUtf8 = postData.utf8CString // it's important to cache this before the closure
+postDataUtf8.withUnsafeBufferPointer { ptr in
+    curlHelperSetOptString(curl, CURLOPT_POSTFIELDS, ptr.baseAddress)
+}
 
 // get verbose debug output
 curlHelperSetOptBool(curl, CURLOPT_VERBOSE, CURL_TRUE)
@@ -49,4 +56,5 @@ if resultCode != CURLE_OK {
     print("something went wrong: " + String(describing: curl_easy_strerror(resultCode)))
 }
 
+curl_slist_free_all(headers); /* free the list again */
 curl_easy_cleanup(curl)
