5b5d7d9577b5f918420f976df720c04610813155
rikner <rikner@web.de>
2017-09-27 23:10:10 +0200

    init multi handle

--- Sources/cCurlTest/main.swift
+++ Sources/cCurlTest/main.swift
@@ -3,18 +3,7 @@ import Foundation
 
 extension String: Error {}
 
-// var read_callback: (@convention(c)
-// (UnsafeMutablePointer<Int8>?, Int, Int, UnsafeMutableRawPointer?) -> Int)! = { dest, size, nmemb, userp in
-//     print("read callback is executed")
-//     return 0
-// }
-
-guard let curl = curl_easy_init() else {
-    print("curl could no be initialised!")
-    throw "Error during curl init"
-}
-
-let serverUrl =  "http://192.168.1.10:3000/graphql"
+let serverUrl =  "http://localhost:3000/graphql"
 let postData = """
 {
 	"query": "query SingleSong($songId: ID!) { song(songId: $songId) { _id } }",
@@ -24,31 +13,62 @@ let postData = """
 }
 """
 
-// set url
-curlHelperSetOptString(curl, CURLOPT_URL, serverUrl)
+func multiPostRequest() throws {
+    guard let multiCurl = curl_multi_init() else {
+        throw "Could not initialise multi curl"
+    }
 
-// set post to true
-curlHelperSetOptBool(curl, CURLOPT_POST, CURL_TRUE)
 
-// set headers
-var headers = curl_slist_append(nil, "Content-Type: application/json")
-headers = curl_slist_append(headers, "charsets: utf-8")
-curlHelperSetOptList(curl, CURLOPT_HTTPHEADER, headers)
-
-// set post data
-let postDataUtf8 = postData.utf8CString // it's important to cache this before the closure
-postDataUtf8.withUnsafeBufferPointer { ptr in
-    curlHelperSetOptString(curl, CURLOPT_POSTFIELDS, ptr.baseAddress)
+    // curl_multi_remove_handle(multiCurl, curl)
+    // curl_easy_cleanup(curl)
+    curl_multi_cleanup(multiCurl)
 }
 
-// set verbose debug output to true
-curlHelperSetOptBool(curl, CURLOPT_VERBOSE, CURL_TRUE)
+try multiPostRequest()
 
-// perform request
-let resultCode = curl_easy_perform(curl)
-if resultCode != CURLE_OK {
-    print("something went wrong: " + String(describing: curl_easy_strerror(resultCode)))
+
+// var read_callback: (@convention(c)
+// (UnsafeMutablePointer<Int8>?, Int, Int, UnsafeMutableRawPointer?) -> Int)! = { dest, size, nmemb, userp in
+//     print("read callback is executed")
+//     return 0
+// }
+
+func easyPostRequest() throws {
+
+    guard let curl = curl_easy_init() else {
+        print("curl could no be initialised!")
+        throw "Error during curl init"
+    }
+
+    // set url
+    curlHelperSetOptString(curl, CURLOPT_URL, serverUrl)
+
+    // set post to true
+    curlHelperSetOptBool(curl, CURLOPT_POST, CURL_TRUE)
+
+    // set headers
+    var headers = curl_slist_append(nil, "Content-Type: application/json")
+    headers = curl_slist_append(headers, "charsets: utf-8")
+    curlHelperSetOptList(curl, CURLOPT_HTTPHEADER, headers)
+
+    // set post data
+    let postDataUtf8 = postData.utf8CString // it's important to cache this before the closure
+    postDataUtf8.withUnsafeBufferPointer { ptr in
+        curlHelperSetOptString(curl, CURLOPT_POSTFIELDS, ptr.baseAddress)
+    }
+
+    // set verbose debug output to true
+    curlHelperSetOptBool(curl, CURLOPT_VERBOSE, CURL_TRUE)
+
+    // perform request
+    let resultCode = curl_easy_perform(curl)
+    if resultCode != CURLE_OK {
+        print("something went wrong: " + String(describing: curl_easy_strerror(resultCode)))
+    }
+
+    curl_slist_free_all(headers)
+    curl_easy_cleanup(curl)
+
 }
 
-curl_slist_free_all(headers)
-curl_easy_cleanup(curl)
+// try easyPostRequest()
